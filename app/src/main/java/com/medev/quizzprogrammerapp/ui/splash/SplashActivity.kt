5bb236c0ecdc1abee512e9efdbf6ffd4d8b427c3
MeydhiXploit <meydhimeydhi@gmail.com>
2024-08-25 07:39:59 -0700

    feat : splash activity

---
+++ app/src/main/java/com/medev/quizzprogrammerapp/ui/splash/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.medev.quizzprogrammerapp.ui.splash
+
+import android.content.Intent
+import  android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.medev.quizzprogrammerapp.R
+import com.medev.quizzprogrammerapp.ui.main.MainActivity
+import kotlinx.coroutines.delay
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        delayAndGotoMain()
+    }
+
+    private fun delayAndGotoMain() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        },1200)
+    }
+}
