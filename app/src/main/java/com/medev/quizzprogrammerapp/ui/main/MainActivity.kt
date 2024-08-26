0c15d7b3b7128965c99e58801bf42c68ac14d8cc
MeydhiXploit <meydhimeydhi@gmail.com>
2024-08-25 20:38:22 -0700

    feat Mainactivity and bg_btn_primary and layout design ui

--- app/src/main/java/com/medev/quizzprogrammerapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/medev/quizzprogrammerapp/ui/main/MainActivity.kt
@@ -6,16 +6,27 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.medev.quizzprogrammerapp.R
+import com.medev.quizzprogrammerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        onClick()
+    }
+
+    private fun onClick() {
+        mainBinding.btnPlay.setOnClickListener {
+
         }
     }
 }
