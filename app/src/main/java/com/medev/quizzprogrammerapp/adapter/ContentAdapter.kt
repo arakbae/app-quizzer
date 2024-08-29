5872073f0b3db45c1bd8135c72cf0b1394b4e637
MeydhiXploit <meydhimeydhi@gmail.com>
2024-08-29 08:56:25 -0700

    feat: content adapter

---
+++ app/src/main/java/com/medev/quizzprogrammerapp/adapter/ContentAdapter.kt
@@ -0,0 +1,49 @@
+package com.medev.quizzprogrammerapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.medev.quizzprogrammerapp.databinding.ItemContentBinding
+import com.medev.quizzprogrammerapp.model.Content
+
+class ContentAdapter:RecyclerView.Adapter<ContentAdapter.ViewHolder>() {
+
+    private var contents = mutableListOf<Content>()
+
+    class ViewHolder(private val itemContentBinding: ItemContentBinding)
+        :RecyclerView.ViewHolder(itemContentBinding.root) {
+        fun bindItem(content: Content) {
+            itemContentBinding.tvQuiz.text = content.body
+            if (content.image != null){
+                itemContentBinding.ivQuiz.visibility = View.VISIBLE
+                Glide.with(itemView)
+                    .load(content.image)
+                    .placeholder(android.R.color.darker_gray)
+                    .into(itemContentBinding.ivQuiz)
+            }else{
+                itemContentBinding.ivQuiz.visibility = View.GONE
+            }
+
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemContentBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = contents.size
+
+    fun setData(contents:MutableList<Content>){
+        this.contents = contents
+        notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindItem(contents[position])
+    }
+}
