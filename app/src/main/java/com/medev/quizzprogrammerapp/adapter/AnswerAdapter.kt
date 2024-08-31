a9da9034e9d7a65261314e923718f50ff86ec3a2
MeydhiXploit <meydhimeydhi@gmail.com>
2024-08-31 07:07:41 -0700

    feat: feature

---
+++ app/src/main/java/com/medev/quizzprogrammerapp/adapter/AnswerAdapter.kt
@@ -0,0 +1,51 @@
+package com.medev.quizzprogrammerapp.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.medev.quizzprogrammerapp.R
+import com.medev.quizzprogrammerapp.databinding.ItemAnswerBinding
+import com.medev.quizzprogrammerapp.model.Answer
+
+class AnswerAdapter:RecyclerView.Adapter<AnswerAdapter.ViewHolder>() {
+
+    private var answers = mutableListOf<Answer>()
+
+    class ViewHolder(private val itemAnswerBinding: ItemAnswerBinding):
+        RecyclerView.ViewHolder(itemAnswerBinding.root) {
+        fun bindItem(answer: Answer) {
+            itemAnswerBinding.tvOption.text = answer.option
+            itemAnswerBinding.tvAnswer.text = answer.answer
+            if (answer.isClick!!){
+                activeCheckAnswer()
+            }else{
+                inActiveCheckAnswer()
+            }
+        }
+
+        private fun inActiveCheckAnswer() {
+            itemAnswerBinding.ivCheckAnswer.setImageResource(R.drawable.baseline_panorama_fish_eye_24)
+        }
+
+        private fun activeCheckAnswer() {
+            itemAnswerBinding.ivCheckAnswer.setImageResource(R.drawable.baseline_check_circle_24)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemAnswerBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = answers.size
+
+    fun setData(answer: MutableList<Answer>){
+        this.answers = answers
+        notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindItem(answers[position])
+    }
+}
